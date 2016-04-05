0bfd330cf4a2c77a5b562505162d24b75ce221cb
user <user@user151>
2016-04-05 19:18:47 +0900

    Create multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,9 +13,14 @@ public class Calculator {
 		return x-y;
 	}
 	
+	public int multiply(int x, int y){
+		return x*y;
+	}
+	
 	public static void main(String[] args){
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(1, 2));
 		System.out.println(cal.sub(1, 2));
+		System.out.println(cal.multiply(5, 6));
 	}
 }
