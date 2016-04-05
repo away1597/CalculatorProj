ba48ff2e9008f8d764c27ce67889faea0a398c72
user <user@user151>
2016-04-05 19:21:21 +0900

    Create divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -17,10 +17,15 @@ public class Calculator {
 		return x*y;
 	}
 	
+	public double divide(int x, int y){
+		return (double)x/(double)y;
+	}
+	
 	public static void main(String[] args){
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(1, 2));
 		System.out.println(cal.sub(1, 2));
 		System.out.println(cal.multiply(5, 6));
+		System.out.println(cal.divide(6, 5));
 	}
 }
